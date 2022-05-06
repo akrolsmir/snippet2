9121e853b81cd1095373f48f024a9faf350937dc
Austin Chen <akrolsmir@gmail.com>
2022-05-06 11:14:56 -0400

    Add placeholder

---
+++ placeholder.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.header("Streamlit Snippets")
+st.write(
+    """Snippets let you quickly try out and share bits of Streamlit code!
+
+1. Write your own snippet 👉
+2. Preview the result (right here!)
+3. Click "Share your work" to get a shareable URL 🎉
+"""
+)
+
